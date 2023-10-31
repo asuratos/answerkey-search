b246b0e21a4f6708695cf5cb5ca88a20bca99d86
Aldrich Suratos <cyurio@gmail.com>
2023-10-31 21:20:36 +0900

    first fully working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use itertools::Itertools;
-use std::io::{stdin, Read};
+use core::fmt;
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::{stdin, BufWriter, Read, Write};
 
-#[derive(Clone, Copy, PartialEq, Debug)]
+use itertools::Itertools;
+
+#[derive(PartialOrd, Ord, Clone, Copy, PartialEq, Eq, Debug)]
 enum Answer {
     A,
     B,
     C,
     D,
+}
+
+impl fmt::Display for Answer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Answer::A => write!(f, "A"),
+            Answer::B => write!(f, "B"),
+            Answer::C => write!(f, "C"),
+            Answer::D => write!(f, "D"),
+        }
+    }
 }
 
 impl From<char> for Answer {
@@ -16,20 +32,38 @@ impl From<char> for Answer {
             'B' => Answer::B,
             'C' => Answer::C,
             'D' => Answer::D,
-            _ => panic!("Invalid letter"),
+            _ => panic!("Invalid letter: {}", value),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Debug, Clone)]
 struct QuizAttempt {
     answers: Vec<Answer>,
     score: i32,
 }
 
-#[derive(Debug, Clone)]
+impl PartialOrd for QuizAttempt {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.score.cmp(&other.score))
+    }
+}
+
+impl Ord for QuizAttempt {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.score.cmp(&other.score)
+    }
+}
+
+#[derive(PartialEq, PartialOrd, Ord, Eq, Debug, Clone)]
 struct AnswerKey {
     answers: Vec<Answer>,
+}
+
+impl AnswerKey {
+    fn as_string(&self) -> String {
+        self.answers.iter().map(|ans| ans.to_string()).collect()
+    }
 }
 
 struct AnswerKeySet {
@@ -50,7 +84,13 @@ impl From<Vec<AnswerKey>> for AnswerKeySet {
 
 impl QuizAttempt {
     fn from_string(string: &str, score: i32) -> QuizAttempt {
-        // TODO: Panic with score > len
+        if string.len() < score as usize {
+            panic!(
+                "Impossible score: {} with test length {}",
+                score,
+                string.len()
+            )
+        }
         QuizAttempt {
             answers: string
                 .to_uppercase()
@@ -61,6 +101,15 @@ impl QuizAttempt {
         }
     }
 
+    fn from_list(list: &[&str]) -> QuizAttempt {
+        QuizAttempt::from_string(
+            list[0],
+            list[1]
+                .parse::<i32>()
+                .expect(&format!("Score is not a number! {}", list[1])),
+        )
+    }
+
     fn check(&self, key: &AnswerKey) -> bool {
         if self.answers.len() != key.answers.len() {
             panic!("Unmatched lengths!");
@@ -68,14 +117,12 @@ impl QuizAttempt {
         self.answers
             .iter()
             .zip(&key.answers)
-            .map(|(&x, &y)| x == y)
-            .map(|r| if r { 1 } else { 0 })
+            .map(|(&x, &y)| if x == y { 1 } else { 0 })
             .sum::<i32>()
             == self.score
     }
 
     fn generate_valid_set(&self) -> AnswerKeySet {
-        // let mut valid_keys: Vec<AnswerKey> = vec![];
         let num_mistakes = self.answers.len() - self.score as usize;
 
         let mut answers_to_try: Vec<Vec<&Answer>> = [Answer::A, Answer::B, Answer::C, Answer::D]
@@ -83,13 +130,13 @@ impl QuizAttempt {
             .combinations_with_replacement(num_mistakes)
             .flat_map(|comb| comb.into_iter().permutations(num_mistakes))
             .collect();
+        answers_to_try.sort();
         answers_to_try.dedup();
 
-        AnswerKeySet::from(
-            (0..self.answers.len())
+        AnswerKeySet::from({
+            let mut small_set = (0..self.answers.len())
                 .combinations(num_mistakes)
                 .flat_map(|possible_mistakes| {
-                    // println!("{:?}", possible_mistakes);
                     let mut small_key = vec![];
                     // generate 2^n solutions
                     for ans in &answers_to_try {
@@ -99,53 +146,100 @@ impl QuizAttempt {
                             this_key[add] = a.clone();
                         }
 
-                        // println!("{:?}", this_key);
                         small_key.push(AnswerKey::from(this_key));
                     }
 
                     small_key
                 })
-                .collect::<Vec<AnswerKey>>(),
-        )
+                .collect::<Vec<AnswerKey>>();
+            small_set.sort();
+            small_set.dedup();
+            small_set
+        })
     }
 }
 
 impl AnswerKeySet {
-    fn reduce(mut self, attempt: QuizAttempt) -> AnswerKeySet {
+    fn reduce(mut self, attempt: &QuizAttempt) -> AnswerKeySet {
         self.keys = self
             .keys
             .iter()
             .filter(|k| attempt.check(k))
             .cloned()
             .collect();
+
         self
+    }
+
+    fn save_to_file(&self, filename: &str) {
+        let mut current_path = env::current_exe().expect("Could not get current path");
+        current_path.pop();
+        current_path.push(filename);
+
+        let f = File::create(current_path).expect("Could not create output file!");
+        let mut f = BufWriter::new(f);
+
+        self.keys.iter().for_each(|key| {
+            writeln!(f, "{}", key.as_string()).expect("Could not write to file!");
+        });
     }
 }
 
-fn main() {
-    // TODO: Parse from txt
+fn extract_attempts_from_file(filename: &str) -> Vec<QuizAttempt> {
+    let mut current_path = env::current_exe().expect("Could not get current path");
+    current_path.pop();
+    current_path.push(filename);
 
-    // TODO: Sort from highest to lowest score
+    let mut loaded_attempts: Vec<QuizAttempt> = fs::read_to_string(current_path)
+        .expect(&format!("Could not read file {}", filename))
+        .lines()
+        .map(|str| QuizAttempt::from_list(&str.split(",").collect::<Vec<&str>>()))
+        .collect();
 
-    let attempt = QuizAttempt::from_string("BCABBDBCAD", 5);
+    // sanity checks
+    // quiz attempts must all have the same length
+    let lens: Vec<usize> = loaded_attempts
+        .iter()
+        .map(|att| att.answers.len())
+        .collect();
 
-    // TODO: fold(attmpt1, |acc, x| acc.reduce(x))
-
-    let mut answerset = attempt.generate_valid_set();
-    answerset = answerset.reduce(QuizAttempt::from_string("CCBBCBABAD", 6));
-    answerset = answerset.reduce(QuizAttempt::from_string("BCDBACABAD", 6));
-    answerset = answerset.reduce(QuizAttempt::from_string("BCCBCAADAD", 6));
-    answerset = answerset.reduce(QuizAttempt::from_string("BCDBCBACAA", 6));
-    answerset = answerset.reduce(QuizAttempt::from_string("BCDBBDBBBD", 5));
-    answerset = answerset.reduce(QuizAttempt::from_string("CCBBBAACBD", 4));
-    answerset = answerset.reduce(QuizAttempt::from_string("CCBBACBBAB", 3));
-
-    println!("{}", answerset.keys.len());
-
-    for ans in answerset.keys {
-        println!("{:?}", ans.answers);
+    if &lens.iter().min() != &lens.iter().max() {
+        panic!("The lengths of the answers are not all the same!")
     }
 
+    // sort by score
+    loaded_attempts.sort();
+    loaded_attempts.reverse();
+
+    loaded_attempts
+}
+
+fn main() {
+    println!("Reading attempts from file: attempts.txt...");
+    let base = extract_attempts_from_file("attempts.txt");
+
+    println!(
+        "Loaded {} answers of length {}",
+        base.len(),
+        base[0].answers.len()
+    );
+
+    println!("Searching for possible answers (This could take a while)...");
+
+    let highest = base[0].generate_valid_set();
+
+    let answerset = &base[1..base.len()]
+        .iter()
+        .fold(highest, |ans_set, att| ans_set.reduce(att));
+
+    println!(
+        "Found {} possible solutions! Writing to possible_answers.txt...",
+        answerset.keys.len()
+    );
+
+    answerset.save_to_file("possible_answers.txt");
+
+    println!("Press any key to end...");
     stdin().read(&mut [0]).unwrap();
 }
 
